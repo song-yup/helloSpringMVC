0ad3a64dc7ad765140179097df9449da4c5cde4b
song-yup <songsy0107@gmail.com>
2024-04-03 15:11:26 +0900

    add spring security

--- src/main/java/kr/ac/hansung/cse/filter/TestFilter.java
+++ src/main/java/kr/ac/hansung/cse/filter/TestFilter.java
@@ -5,7 +5,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-@WebFilter("/*")
+@WebFilter("/*")    // root 이하의 모든 것을 가로챈다
 public class TestFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -14,9 +14,9 @@ public class TestFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println( ((HttpServletRequest) request).getRequestURL() );
+        System.out.println( ((HttpServletRequest) request).getRequestURL() );   // 여기서 사용자의 requesturl을 넣어줬다, log에 url이 찍히는 이유는 이 문장 때문이다
 
-        // pass the request along the filter chain
+        // pass the request along the filter chain  (그 다음 filter chain으로 넘어간다)
         chain.doFilter(request, response);
     }
 
